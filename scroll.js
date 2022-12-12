bd7d3f0509770f735de1cedc4cb2c0ca3bbaf14f
Samuel Hernan <samuelhernanmoreno@gmail.com>
2022-12-12 12:01:22 +0100

    Mejora del formato

--- scroll.js
+++ scroll.js
@@ -1,11 +1,8 @@
 window.addEventListener('scroll', function() {
     let navbar = document.querySelector('nav');
-    let image = document.querySelector('.image');
     if (window.pageYOffset > 0) {
         navbar.classList.add('sticky');
-        image.classList.add('sticky)')
     } else {
         navbar.classList.remove('sticky');
-        image.classList.remove('sticky)')
     }
 });
