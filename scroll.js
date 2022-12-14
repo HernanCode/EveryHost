a04aabf28b1a37fe536f4af8bc0925a06f8e11ff
Samuel Hernan <samuelhernanmoreno@gmail.com>
2022-12-14 21:22:00 +0100

    Agregado un efecto de scroll para el logo Agregados enlaces de botones Diseño más responsive

--- scroll.js
+++ scroll.js
@@ -1,8 +1,14 @@
 window.addEventListener('scroll', function() {
     let navbar = document.querySelector('nav');
+    const img1 = document.getElementById('imgwhite');
+    const img2 = document.getElementById('imgblue');
     if (window.pageYOffset > 0) {
         navbar.classList.add('sticky');
+        img1.style.display = 'none';
+        img2.style.display = 'flex';
     } else {
         navbar.classList.remove('sticky');
+        img2.style.display = 'none';
+        img1.style.display = 'flex';
     }
 });
