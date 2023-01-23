7d93e3d2e443e80387913fc6745de65e1be45f8f
HernanCode <samuelhernanmoreno@gmail.com>
2023-01-23 13:23:56 +0100

    Un saludin pa ti, de nada Se añade que el texto de la pagina principal se escriba de forma sequencial en vez de en paralelo;

--- writter.js
+++ writter.js
@@ -1,16 +1,24 @@
-function writeText(element) {
-    const text = element.getAttribute("data-text");
-    element.classList.add("written");
+function writeTitle() {
+    element = document.getElementById("prueba201");
+    text = element.getAttribute("data-text");
     for (let i = 0; i < text.length; i++) {
         setTimeout(function() {
             element.innerHTML += text[i];
         }, i * 40);
     }
+    setTimeout(writeSubtext, text.length * 40);
 }
-const titleElements = document.querySelectorAll(".write-text");
-titleElements.forEach(function(titleElement) {
-    if (!titleElement.classList.contains("written")) {
-        writeText(titleElement);
 
+function writeSubtext() {
+    element1 = document.getElementById("prueba200");
+    text1 = element1.getAttribute("data-text");
+    for (let a = 0; a < text1.length; a++) {
+        setTimeout(function() {
+            element1.innerHTML += text1[a];
+        }, a * 40);
     }
-});
+
+}
+
+
+writeTitle();
